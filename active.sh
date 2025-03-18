5f5c2fbaaa785645de95a0f15f8e40f26c2792d9
shuinoo <38451598+shuinoo@users.noreply.github.com>
2025-03-18 15:13:13 +0800

    Update active.sh

--- active.sh
+++ active.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+current_time=date
 hy2=$(pp | grep "serv00sb run | wc -l)
 if [[ $hy2 -eq 0 ]];then
   cd /home/shuinoo/serv00-play/singbox
   ./serv00sb run -c ./config.json
-  echo "`date` hy2 is deaded..." >> /home/shuinoo/hy_runing.log
+  echo "$current_time hy2 is deaded..." >> /home/shuinoo/hy_runing.log
 else
-  echo "`date` hy2 is still running..." >> /home/shuinoo/hy_runing.log
+  echo "$current_time hy2 is still running..." >> /home/shuinoo/hy_runing.log
 fi
 
   
