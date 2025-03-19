5bc6f54afa193f1f5b0dd7fbca153d345e19ef19
shuinoo <38451598+shuinoo@users.noreply.github.com>
2025-03-19 11:06:30 +0800

    Update active.sh

--- active.sh
+++ active.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-current_time=$(date)
+current_time=$(date -z Asia/Shanghai)
 hy2=$(ps aux | grep "serv00sb run -c ./config.json" | grep -v "grep" | wc -l | awk '{print $1}')
 if [ $hy2 -eq 0 ];then
   cd /home/shuinoo/serv00-play/singbox
